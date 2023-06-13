3e36f3ac5aad545cbda5594914f64450b0f344d7
Mark Lane <msl2466@gmail.com>
2023-06-13 11:43:18 +0100

    Add my name

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -13,6 +13,7 @@ public class App
         System.out.println(sayHelloToSomeone("Bob"));
         System.out.println(sayHelloToSomeone("Charlie"));
         System.out.println(sayHelloToSomeone("Zena"));
+        System.out.println(sayHelloToSomeone("Mark"))
         System.out.println(sayGoodbye());
     }
 
